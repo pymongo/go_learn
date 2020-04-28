e14617dd19ca0cc33ffb7a28a3c566f7f9db724d
吴翱翔 <pylint@yandex.com>
2020-04-28 10:12:31 +0800

    完成了go语言的枚举类型练习

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+const (
+	gender_male = iota
+	gender_femail = iota
+)
+
+const (
+	trade_type_buy = iota
+	trade_type_sell
+)
 
 func main() {
-	fmt.Println("Hello World")
+	gender := gender_femail
+	trade_type := trade_type_sell
+	fmt.Println(gender) // 1
+	fmt.Println(trade_type) // 1
 }
+
+func add(x int32, y int32) int32 {
+	return x + y
+}
+
